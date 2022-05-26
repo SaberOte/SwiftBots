eeb14c87564b5b6abacc71eb8f3cec7888dd4176
PolkaDott <andrey.gurt@yandex.ru>
2022-05-26 19:44:49 +0500

    закончен менеджер крона

---
+++ src/cronmanager.py
@@ -0,0 +1,50 @@
+import crontab, getpass, os, configparser, uuid
+def add(plugin, task, freq, proj_path):
+  cron = crontab.CronTab(user=getpass.getuser())
+  task_name = f'{plugin}|{task}'
+  config_path = os.path.join(proj_path, "resources/config.ini") 
+  
+  # добавить задачу в конфиг
+  config = configparser.ConfigParser()
+  config.read(config_path)
+  if 'Tasks' not in config.sections():
+    config.add_section("Tasks")
+  task_id = str(uuid.uuid4())
+  config.set('Tasks', task_name, task_id)
+  with open(config_path, 'w') as file:
+    config.write(file)
+
+  # очистить старые задачи с таким же именем
+  for job in cron:
+    if job.comment == task_name:
+      cron.remove(job)
+  
+  # сделать посекундое ожидание
+  sec = 0
+  seconds = []
+  while not sec or sec % 60:
+    seconds.append(sec)
+    sec += freq
+  
+  cronstart_path = os.path.join(proj_path, "src/cronstart.py")
+  for i in seconds:
+    command = f'sleep {i}; python3 "{cronstart_path}" "{task_id}" "{config_path}"'
+    tab = cron.new(comment=task_name, command=command)
+    tab.minute.every(seconds[-1]//60+1)
+  cron.write()
+
+def remove(plugin, task, proj_path):
+  config_path = os.path.join(proj_path, "resources/config.ini") 
+  cron = crontab.CronTab(user=getpass.getuser())
+  task_name = f'{plugin}|{task}'
+  for job in cron:
+    if job.comment == task_name:
+      cron.remove(job)
+  cron.write()
+
+  # убрать задачу из конфигa
+  config = configparser.ConfigParser()
+  config.read(config_path)
+  config.remove_option('Tasks', task_name)
+  with open(config_path, 'w') as file:
+    config.write(file)
--- src/cronstart.py
+++ src/cronstart.py
@@ -1,13 +1,20 @@
-import sys
-from communicator import Communicator
+import sys, communicator, configparser
+
 argv = sys.argv[1:]
-plugin = argv[0]
-task = argv[1]
-names_path = argv[2]
-comm = Communicator(names_path, 'crontest')
-data = { 
-  "plugin" : plugin,
-  "task" : task,
-}
-comm.send(str(data), 'listener')
-comm.close()
+task_id = argv[0]
+config_path = argv[1]
+
+#получить имя плагина и задачи
+config = configparser.ConfigParser()
+config.read(config_path)
+task_name = None
+for i in config.items('Tasks'):
+  if i[1] == task_id:
+    task_name = i[0]
+    break
+if task_name:
+  comm = communicator.Communicator(config_path, 'cron')
+  comm.send(task_name, 'listener')
+  comm.close()
+else:
+  pass # задача уже удалена
