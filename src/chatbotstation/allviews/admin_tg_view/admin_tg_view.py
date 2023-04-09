8a5b87a27817bba9ed216367f76cb5c2b42d33db
PolkaDott <andrey.gurt@yandex.ru>
2023-04-09 16:39:02 +0500

    Renamed

---
+++ src/chatbotstation/allviews/admin_tg_view/admin_tg_view.py
@@ -0,0 +1,11 @@
+from ...templates.telegram_view import TelegramView
+from ...config import read_config
+
+
+class AdminTgView(TelegramView):
+    plugins = ['admin_panel']
+
+    def __init__(self):
+        config = read_config('credentials.ini')
+        self.TOKEN = config['AdminTgView']['token']
+        self.admin = config['AdminTgView']['admin']
