0a826efa26742d94f3acebabe34090611f69d39f
Andrey <andrey.gurt@yandex.ru>
2023-05-22 02:20:26 +0500

    Мусор

--- src/botcore/upbot.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-fullpath = os.path.abspath(__file__)
-app = fullpath.split('/')[-1]
-fold = fullpath.split('/')[-2]
-path = fullpath[:-(len(fold)+len(app)+2)]
-os.chdir(path)
-os.system('nohup python3 {} > ./{}/resources/launchlogs.txt 2>&1 &'.format(fold, fold))
