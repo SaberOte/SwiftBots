3e7adce65ff085041678ec84057cc38591fcbf9e
Andrey <andrey.gurt@yandex.ru>
2023-05-22 02:06:53 +0500

    Загружена папка examples

---
+++ src/examples/controllers/say_hello.py
@@ -0,0 +1,29 @@
+"""
+Example of some controller that can just send some dumb messages
+
+1. `cmds` property is a dict where:
+   - Key is a command that user pass
+   - Value is a function that executes the command.
+   Usage example:
+   (user): hello
+   (bot): Yo!
+2. `any` is just a pipe to load there all of query's data.
+It's used by only when no one command is not accepted
+"""
+from src.botcore.templates.base_multiple_users_view import BaseMultipleUsersView
+from src.botcore.templates.base_controller import BaseController
+
+class SayHello(BaseController):
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    def say_hi(self, view: BaseMultipleUsersView, context: dict):
+        view.reply('Yo!', context)
+
+    def say_truth(self, view: BaseMultipleUsersView, context: dict):
+        view.reply('PHP is the best PL ever!', context)
+
+    cmds = {
+        "hello": say_hi,
+    }
+    any = say_truth
