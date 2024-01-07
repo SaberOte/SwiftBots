9503b90ac1850c70a8f118d9c79c08ed3f56b764
Andrey <andrey.gurt@yandex.ru>
2024-01-07 08:23:47 +0500

    добавлены тесты

---
+++ tests/test_basic_view.py
@@ -0,0 +1,44 @@
+import asyncio
+
+from swiftbots.controllers import Controller
+from swiftbots.views import BasicView
+from swiftbots import initialize_app
+from swiftbots.admin_utils import shutdown_app
+
+
+global_var = 0
+
+
+class MyBasicView(BasicView):
+
+    async def listen_async(self):
+        while True:
+            await asyncio.sleep(0)
+            test_value = 69
+            yield self.PreContext(test_value)
+
+    async def change_var_async(self, value):
+        await asyncio.sleep(0)
+        global global_var
+        global_var = value
+        shutdown_app()
+
+
+class MyController(Controller):
+
+    async def default(self, view: MyBasicView, context: MyBasicView.Context):
+        mes: int = context.raw_message
+        await view.change_var_async(mes + 2)
+
+
+class TestBasicView:
+
+    def test_default_handler(self):
+        app = initialize_app()
+
+        app.add_bot(MyBasicView, [MyController])
+
+        app.run()
+
+        global global_var
+        assert global_var == 71
