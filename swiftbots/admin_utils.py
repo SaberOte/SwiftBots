7546441a01bb40cc0fda54a043477e4475bc5dbe
saberote <andrey.gurt@yandex.ru>
2024-07-21 01:15:27 +0500

    переименовал метод

--- swiftbots/admin_utils.py
+++ swiftbots/admin_utils.py
@@ -14,7 +14,7 @@ def shutdown_app() -> None:
     raise ExitApplicationException("Exited from administrator")
 
 
-async def shutdown_bot_async(bot_name: str) -> bool:
+def shutdown_bot(bot_name: str) -> bool:
     """
     Shutdown the instance. Won't restart.
     If param bot_name is provided, it closes the current task.
